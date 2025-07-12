1116fb882e44cac5e2c765eaf55086d7c7ad54b5
Rafael <rak19722@gmail.com>
2025-07-12 01:14:25 -0600

    final-final

--- app.py
+++ app.py
@@ -695,7 +695,11 @@ def api_properties():
     return jsonify(properties_data)
 
 # --------------------- Run ---------------------
+
+app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
